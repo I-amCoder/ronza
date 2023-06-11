56ac18d4e386f0f3cfad8ba69a3cfedf719ccc49
Junaid Ali <Junaid Ali>
2023-06-11 10:55:55 +0500

    HElo

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -14,7 +14,14 @@ const products = [
   { id: 2, name: "Product 2", categoryId: 2 },
   { id: 3, name: "Product 3", categoryId: 1 },
   { id: 4, name: "Product 4", categoryId: 3 },
-  { id: 5, name: "Product 5", categoryId: 2 },
+  { id: 5, name: "Product 5", categoryId: 3 },
+  { id: 6, name: "Product 6", categoryId: 3 },
+  { id: 7, name: "Product 7", categoryId: 3 },
+  { id: 8, name: "Product 8", categoryId: 3 },
+  { id: 9, name: "Product 9", categoryId: 3 },
+  { id: 10, name: "Product 10", categoryId: 3 },
+  { id: 11, name: "Product 11", categoryId: 3 },
+  { id: 12, name: "Product 12", categoryId: 2 },
   // Add more product data as needed
 ];
 
@@ -46,11 +53,26 @@ const Products = () => {
           </button>
         ))}
       </div>
-      <div className="product-list">
+      <div className="container row">
         {filteredProducts.map((product) => (
-          <div className="product" key={product.id}>
-            <h3>{product.name}</h3>
-            <p>{getCategoryName(product.categoryId)}</p>
+          <div
+            key={product.id}
+            className="col-sm-6 my-2 col-md-4 col-lg-3 col-xl-2"
+          >
+            <div className="product">
+              <div className="image-container">
+                <img
+                  src="https://images.unsplash.com/photo-1511556820780-d912e42b4980?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODQ0Mzl8MHwxfHNlYXJjaHwyfHxwcm9kdWN0c3xlbnwwfHx8fDE2Njk3MzUzMTQ&ixlib=rb-4.0.3&q=80&w=400"
+                  alt=""
+                  className="p-img"
+                />
+              </div>
+              
+              <div className="details">
+                <h5 className="">{product.name}</h5>
+                <p className="mt-3">{getCategoryName(product.categoryId)}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SiteContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/ronza">
         <App />
       </BrowserRouter>
     </SiteContextProvider>
